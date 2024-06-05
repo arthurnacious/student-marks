9b1468b1016a94c55fcfe9d6dee47a7b699c7ffd
Arthurnacious <arthurnacious@gmail.com>
2024-06-05 12:44:59 +0200

    fixed 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,27 +7,25 @@ interface Props {}
 
 const NotFound: FC<Props> = ({}) => {
   return (
-    <PageContainerWrapper
-      trail={
-        <BackButton>
-          <ArrowLeft /> Back
-        </BackButton>
-      }
-    >
-      <div className="flex flex-col items-center justify-center h-[calc(100dvh-13rem)] px-4 md:px-6 space-y-6">
+    <div className="flex flex-col p-10 gap-5">
+      <BackButton>
+        <ArrowLeft /> Back
+      </BackButton>
+      <div className="flex flex-col items-center justify-center px-auto md:px-6 space-y-6">
         <div className="text-center space-y-2">
-          <h1 className="text-9xl font-bold text-slate-200 dark:text-neutral-900">
+          <h1 className="sm:text-9xl text-6xl font-bold text-slate-200 dark:text-neutral-900 font-SpaceGrotesk">
             404
           </h1>
           <p className="text-lg text-gray-400 dark:text-gray-500">
             Oops, the page you&lsquo;re looking for doesn&lsquo;t exist.
           </p>
         </div>
-        <BackButton className="inline-flex h-10 items-center justify-center rounded-md bg-gray-500 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-500 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300">
+        <BackButton className="inline-flex h-10 items-center justify-center rounded-md bg-gray-500 px-2 md:px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-500 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300 text-nowrap">
+          <ArrowLeft />
           Go back home
         </BackButton>
       </div>
-    </PageContainerWrapper>
+    </div>
   );
 };
 
--- src/components/page-container-wrapper.tsx
+++ src/components/page-container-wrapper.tsx
@@ -1,14 +1,21 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 interface Props {
   title?: string;
   trail?: React.ReactNode;
+  className?: string;
   children?: React.ReactNode;
 }
 
-const PageContainerWrapper: React.FC<Props> = ({ title, trail, children }) => {
+const PageContainerWrapper: React.FC<Props> = ({
+  title,
+  trail,
+  className,
+  children,
+}) => {
   return (
-    <div className="flex flex-col p-10 ml-20 w-full gap-5">
+    <div className={cn("flex flex-col p-10 ml-20 w-full gap-5", className)}>
       {!!title && (
         <h1 className="text-4xl text-neutral-200 uppercase">{title}</h1>
       )}
